232e08bf28e0b748c0cec8f5717ef32dd4773a84
Filip Adamczyk <filip@filbot.com>
2013-06-03 19:35:00 -0700

    uncommented code block

--- logic/open.js
+++ logic/open.js
@@ -1,43 +1,42 @@
-// var gpio = require('pi-gpio');
+var gpio = require('pi-gpio');
 
 
 function openSesame() {
 
-    console.log('Lookin good!');
-    // console.log('openSesame function fired');
+    console.log('openSesame function fired');
 
-    // // open pin 16 and set to output
-    // gpio.open(16, "output", function(err) {
+    // open pin 16 and set to output
+    gpio.open(16, "output", function(err) {
 
-    //     if (err) {  // if error stop and console log out
-    //       console.log(err);
-    //     }
-    //     else { // do all the rest
+        if (err) {  // if error stop and console log out
+          console.log(err);
+        }
+        else { // do all the rest
 
-    //         // set pin 16 to high
-    //         gpio.write(16, 1, function() {
+            // set pin 16 to high
+            gpio.write(16, 1, function() {
 
-    //             // open pin 18 and set to output
-    //             gpio.open(18, "output", function(err) {
+                // open pin 18 and set to output
+                gpio.open(18, "output", function(err) {
 
-    //                 if (err) {  // if error stop and close ping 16 and console log out
-    //                     gpio.close(16);
-    //                     console.log(err);
-    //                 }
-    //                 else { // do all the rest
+                    if (err) {  // if error stop and close ping 16 and console log out
+                        gpio.close(16);
+                        console.log(err);
+                    }
+                    else { // do all the rest
 
-    //                     // set pin 18 to high
-    //                     gpio.write(18, 1, function() {
-    //                         module.setInterval(function () {
-    //                             gpio.close(16);  // clost pin 16
-    //                             gpio.close(18);  // clost pin 18
-    //                         },5000);
-    //                     });
-    //                 }
-    //             });
-    //         });
-    //     }
-    // });
+                        // set pin 18 to high
+                        gpio.write(18, 1, function() {
+                            module.setInterval(function () {
+                                gpio.close(16);  // clost pin 16
+                                gpio.close(18);  // clost pin 18
+                            },5000);
+                        });
+                    }
+                });
+            });
+        }
+    });
 
 }
 
