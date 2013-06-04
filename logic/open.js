18150ab45b8d4b05954ea4f34de993d02a9da7ef
Filip Adamczyk <filip@filbot.com>
2013-06-04 09:32:47 -0700

    changed setInterval to setTimeout and set to 500ms

--- logic/open.js
+++ logic/open.js
@@ -41,11 +41,11 @@ function openSesame() {
 
     gpio.open(16, "output", function(err) {         // Open pin 16 for output
         gpio.write(16, 0, function() {              // Set pin 16 low (0)
-            setInterval(function () {
+            setTimeout(function () {
                 gpio.write(16, 1, function() {          // Set pin 16 high (1)
                     gpio.close(16);                     // Close pin 16
                 });
-            },3000);
+            },500);
         });
     });
 
