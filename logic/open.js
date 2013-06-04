77cb7a490c5dc50485bc31875dc6d1164e7ec2c1
Filip Adamczyk <filip@filbot.com>
2013-06-03 21:05:25 -0700

    add set pin low

--- logic/open.js
+++ logic/open.js
@@ -38,9 +38,11 @@ function openSesame() {
     //     }
     // });
     // 
-    gpio.open(16, "output", function(err) {        // Open pin 16 for output
-        gpio.write(16, 1, function() {            // Set pin 16 high (1)
-            gpio.close(16);                        // Close pin 16
+    gpio.open(16, "output", function(err) {         // Open pin 16 for output
+        gpio.write(16, 1, function() {              // Set pin 16 high (1)
+            gpio.write(16, 0, function() {          // Set pin 16 low (0)
+                gpio.close(16);                     // Close pin 16
+            });
     });
 });
 
